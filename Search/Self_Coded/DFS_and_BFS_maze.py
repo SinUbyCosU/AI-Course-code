c52f1fbb36022c2afeda1b0487b59ecd2eed9fac
SinUbyCosU <yadav23@iiserb.ac.in>
2026-01-29 10:57:32 +0530

    code3

--- Search/Self_Coded/DFS_and_BFS_maze.py
+++ Search/Self_Coded/DFS_and_BFS_maze.py
@@ -11,3 +11,15 @@ height=len(lines)
 width=max(len(line) for line in lines)
 
 print(f"height:{height}, width:{width}")
+
+#step 2c find start A and goal B
+
+start =None
+goal=None
+for i in range(height):
+    for j in range(len(lines[i])):
+        if lines[i][j]=='A':
+            start=(i,j)
+        elif lines[i][j]=='B':
+            goal=(i,j)
+    print(f"start:{start},goal:{goal}")
